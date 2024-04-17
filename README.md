57b1bbfcccb555ab3e97b46eafcc5e31c75e1823
mrt <rossanot@users.noreply.github.com>
2024-04-16 20:04:56 -0600

    Initiate project
    - Add principal folders and files

---
+++ README.md
@@ -0,0 +1,6 @@
+# **Object-Relational Mapping Using SQLAlchemy and Spotify Data**
+
+Authors:
+- [Maria Rossano](https://github.com/rossanot/)
+
+This project was inspired by [this Real Python tutorial](https://realpython.com/python-sqlite-sqlalchemy/#working-with-sqlalchemy-and-python-objects). Credits of the code are granted accordingly throughout the project.
---
+++ setup.py
@@ -0,0 +1,23 @@
+"""Adapted from Real Python 
+https://realpython.com/python-sqlite-sqlalchemy/#working-with-sqlalchemy-and-python-objects"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="project",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=[
+        "pandas==2.2.2",
+        "numpy==1.26.4",
+        "Flask==1.1.1",
+        "SQLAlchemy==1.3.13",
+        "Flask-SQLAlchemy==2.4.1",
+        "Flask-Cors==3.0.8",
+        "Flask-Bootstrap4==4.0.2",
+        "Flask-WTF==0.14.3",
+        "python-dateutil==2.9",
+        "python-dotenv==0.10.5",
+        "treelib",
+    ],
+)
