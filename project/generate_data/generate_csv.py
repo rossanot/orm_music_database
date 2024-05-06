bbcffc1bf7af9de5b720b434039d0d309c98e9c6
mrt <rossanot@users.noreply.github.com>
2024-05-06 16:06:13 -0600

    Implement data processing and fix typos
    - Add docs in module functions
    - Fix typos
    - Add `resources` paths for better consumption of PATHs
    - tested via `python project/generate_data/generate_csv.py`

--- project/generate_data/generate_csv.py
+++ project/generate_data/generate_csv.py
@@ -1,8 +1,35 @@
+"""Process the .csv file acquired from Kaggle
+"""
+from typing import List
+from importlib import resources
 import re
 import pandas as pd
 
 
-def get_reduced_df(df: pd.DataFrame):
+def load_csv(
+        path: str) -> pd.DataFrame:
+    """Load .csv file as a pd.DataFrame
+    """
+    return pd.read_csv(path, low_memory=False)
+
+
+def save_csv(
+        path: str,
+        df: pd.DataFrame
+        ) -> None:
+    """Save pd.DataFrame as .csv file
+    """
+    df.to_csv(path, index=False)
+
+
+def get_reduced_df(
+        df: pd.DataFrame
+        ) -> pd.DataFrame:
+    """Reduce dimension of df
+
+    :param df: a pd.DataFrame
+    :return: reduced df, pd.DataFrame
+    """
     columns = ['BL record ID', 'Composer', 'Title',
                'Country of publication',
                'Publication date (standardised)',
@@ -11,15 +38,25 @@ def get_reduced_df(df: pd.DataFrame):
     return df[columns]
 
 
-def drop_nulls(df):
-    """_summary_
+def drop_nulls(
+        df: pd.DataFrame
+        ) -> pd.DataFrame:
+    """Drop rows with null values
 
-    :param df: _description_
+    :param df: a pd.DataFrame
+    :return: reduced df, pd.DataFrame
     """
     return df.dropna().reset_index(drop=True)
 
 
-def assign_dtypes(df):
+def assign_dtypes(
+        df: pd.DataFrame
+        ) -> pd.DataFrame:
+    """Assign column dtypes
+
+    :param df: a pd.DataFrame
+    :return: a pd.DataFrame
+    """
     df['Publication date standardised)'] = \
         df['Publication date (standardised)'].astype(
         'int64')
@@ -30,40 +67,61 @@ def explode_genre(
         df: pd.DataFrame
         ) -> pd.DataFrame:
     """Explode genre column
+
+    :param df: a pd.DataFrame
+    :return: exploded df wrt 'genres'
     """
     df['genres'] = df['Subject/genre terms'].apply(lambda x: x.split(' ;'))
     return df.explode('genres')
 
 
-def _subs_text(text):
+def _subs_text(
+        text: str
+        ) -> List:
+    """Find pattern in text and tag it
+
+    tags: 'pattern' or 'Composer'
+    :param text: Composer column row, str
+    :return: List[text, (`pattern`|'Composer')]
+    """
     pattern = r'\(Music[a-zA-Z\s]*\)$'
     found = re.search(pattern, text)
-    
+
     if found:
         return [text[:found.span()[0]], found.group()]
     else:
-        return [text, 'Compositor']
+        return [text, 'Composer']
 
 
-def simple_composer(df):
-    """Drop '(Musician)' and '(Musical group)'
-    from composer and create 'Composer type'
-    and 'Composer (standardised)' columns
+def simple_composer(
+        df: pd.DataFrame
+        ) -> pd.DataFrame:
+    """Obtain 'Composer std' and 'Composer info'
+    
+    'Composer std' contains `Composer` info
+    'Composer info' contains 'Musician' | 'Musical group'
+    :param df: a pd.DataFrame
+    :return: a pd.DataFrame
     """
     new_composer = df['Composer'].apply(_subs_text)
-    df[['Composer std', 'Composer info']] = pd.DataFrame(new_composer.to_list())
+    df[['Composer std', 'Composer info']] = pd.DataFrame(
+        new_composer.to_list()
+        )
+    
     return df
 
 
 def main():
-    PATH_INPUT = '../project/data/detailedrecords.csv'
-    PATH_OUTPUT = '../project/data/reduced_detailedrecords.csv'
-    df = pd.read_csv(PATH_INPUT)
+    PATH_INPUT = resources.path('project.data',
+                                'detailedrecords.csv')
+    PATH_OUTPUT = resources.path('project.data',
+                                 'reduced_detailedrecords.csv')
+    df = load_csv(PATH_INPUT)
     df = get_reduced_df(df).copy()
     df = drop_nulls(df)
     df = assign_dtypes(df)
     df = simple_composer(df)
-    df.to_csv(PATH_OUTPUT, index=False)
+    save_csv(PATH_OUTPUT, df)
 
 
 if __name__ == '__main__':
