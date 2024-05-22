861f759e8c96ef00cead836cc0ea401f1ffb4d63
mrt <rossanot@users.noreply.github.com>
2024-05-21 19:30:28 -0600

    Update compositor and track table classes
    - Add compisitor and track table classes
    - Single one-to-one relationship between tables
    - To do:
    (i) update tables for the rest of columns in csv dataset
    (ii) implement compositor and track classes in generate_db.py

--- project/modules/models.py
+++ project/modules/models.py
@@ -4,35 +4,19 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-track_artist = Table(
-    'track_artist',
-    Base.metadata,
-    Column('track_id', Integer, ForeignKey('track.track_id')),
-    Column('artist_id', Integer, ForeignKey('artist.artist_id'))
-)
 
-
-class Artist(Base):
-    __tablename__ = 'artist'
-    artist_id = Column(Integer, primary_key=True)
-    artist_name = Column(String)
-    tracks = relationship(
-        'Track', secondary=track_artist, back_populates='artists'
-        )
+class Composer(Base):
+    __tablename__ = 'composer'
+    composer_id = Column(Integer, primary_key=True)
+    composer_name = Column(String)
+    tracks = relationship('Track', backref=backref('composer'))
 
 
 class Track(Base):
     __tablename__ = 'track'
     track_id = Column(Integer, primary_key=True)
-    scale_id = Column(Integer, ForeignKey('scale.scale_id'))
+    composer_id = Column(Integer, ForeignKey('composer.composer_id'))
+    track_bl_id = Column(Integer)
     track_name = Column(String)
-    artists = relationship(
-        'Artist', secondary=track_artist, back_populates='tracks'
-        )
 
 
-class Scale(Base):
-    __tablename__ = 'scale'
-    scale_id = Column(Integer, primary_key=True)
-    scale_key = Column(String)
-    tracks = relationship('Track', backref=backref('scale'))
