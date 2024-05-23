5935512b1760ea65e8ee1c648ecf761cfe4b7835
m-beans <rossanot@users.noreply.github.com>
2024-05-23 16:54:48 -0600

    Implement Country table
    - Add Country table class
    - Add Country table population

--- project/modules/models.py
+++ project/modules/models.py
@@ -16,7 +16,17 @@ class Track(Base):
     __tablename__ = 'track'
     track_id = Column(Integer, primary_key=True)
     composer_id = Column(Integer, ForeignKey('composer.composer_id'))
+    country_id = Column(Integer, ForeignKey('country.country_id'))
     track_bl_id = Column(Integer)
     track_name = Column(String)
+
+
+class Country(Base):
+    __tablename__ = 'country'
+    country_id = Column(Integer, primary_key=True)
+    country_name = Column(String)
+    tracks = relationship('Track', backref=backref('country'))
+
+
 
 
